0a58889ce01b51f93764c82cb992618aaf6ce5c8
Saurabha Arya <arya.saurabha@gmail.com>
2023-09-29 06:24:39 -0400

    Revert "Exercise 1.1"
    This reverts commit cfa5232206b9853e648dabbea462081673afc14e.

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,57 +1,27 @@
-const Header = (props) => {
-  return (
-    <>
-      <h1>{props.course}</h1>
-    </>
-  );
-}
-
-const Content = (props) => {
-  return (
-    <>
-      {props.topics.map((topic) => (
-        <>
-          <p>{topic.name} {topic.exercises}</p>
-        </>
-      ))}
-    </>
-  );
-}
-
-const Total = (props) => {
-  let sum = 0;
-  props.total.forEach(topic => sum += topic.exercises);
-  return (
-    <>
-      <p>Number of exercises {sum}</p>
-    </>
-  );
-}
-
 const App = () => {
   const course = 'Half Stack application development'
-  const topics = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ];
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
     <div>
-      <Header course = {course}/>
-      <Content topics={topics}/>
-      <Total total={topics}/>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
-}   
+}
 
 export default App
